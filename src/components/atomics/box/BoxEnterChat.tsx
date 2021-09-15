2dd5ebe5fbf776931defb887c7b4213453d8878e
Dekguh <kadekteguhmahesa@gmail.com>
2021-09-15 10:59:36 +0800

    create input form chat

---
+++ src/components/atomics/box/BoxEnterChat.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { FiChevronsRight } from 'react-icons/fi'
+
+interface Props {
+    nameSupport: string;
+    isOnline: boolean;
+    clickMinimize: React.MouseEventHandler;
+    clickSend: React.MouseEventHandler;
+}
+
+const BoxEnterChat: React.FC<Props> = ({ nameSupport, isOnline, clickMinimize, clickSend }) => {
+    return (
+        <div className='shadow-primary bg-white rounded-md w-310px border border-gray-100 relative'>
+            <div className='bg-blue-500 py-3 px-5 rounded-t-md'>
+                <div className='flex flex-row flex-nowrap items-center'>
+                    <div className='flex-grow flex-shrink'>
+                        <h6 className='mb-0 text-white font-medium'>{nameSupport}</h6>
+                        {isOnline
+                            ? (<span className='text-green-400'>online</span>)
+                            : (<span className='text-red-400'>offline</span>)
+                        }
+                    </div>
+
+                    <div className='flex-grow-0 flex-shrink'>
+                        <button onClick={clickMinimize} className='p-0 border border-white py-1 px-1 rounded-sm text-white'>
+                            <i>
+                                <FiChevronsRight />
+                            </i>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div className='pt-5 pb-5 px-5'>
+                <div className='mb-3'>
+                    <label className='label'>Nama</label>
+                    <input className='input-primary py-2 pl-5 pr-9 text-sm' placeholder='nama anda' />
+                </div>
+
+                <div className='mb-3'>
+                    <label className='label'>e-mail</label>
+                    <input type='email' className='input-primary py-2 pl-5 pr-9 text-sm' placeholder='email anda' />
+                </div>
+
+                <div className='mb-3'>
+                    <label className='label'>pesan</label>
+                    <textarea className='input-primary py-2 pl-5 pr-9 text-sm' placeholder='pesan' rows={4}></textarea>
+                </div>
+
+                <div className='mb-3'>
+                    <button onClick={clickSend} className='button-primary py-2 px-5 text-sm'>kirim</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BoxEnterChat
--- src/components/molecules/Contact/ChatContainer.tsx
+++ src/components/molecules/Contact/ChatContainer.tsx
@@ -1,17 +1,26 @@
 import React, { useState } from 'react'
 import { FiHeadphones } from 'react-icons/fi'
 import BoxChatForm from '../../atomics/box/BoxChatForm'
+import BoxEnterChat from '../../atomics/box/BoxEnterChat'
 
 const ChatContainer: React.FC = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false)
+    const [isChatBox, setIsChatBox] = useState<boolean>(false)
     return (
         <div className='fixed bottom-5 right-5 z-40 overflow-hidden'>
             <div className={`relative ${isOpen ? 'right-0' : '-right-96'} duration-1000`}>
-                <BoxChatForm
+                {isChatBox
+                ? (<BoxChatForm
                     nameSupport='Amanda Tan'
                     isOnline={true}
                     clickMinimize={e => setIsOpen(!isOpen)}
-                />
+                />)
+                : (<BoxEnterChat
+                    nameSupport='Amanda Tan'
+                    isOnline={true}
+                    clickMinimize={e => setIsOpen(!isOpen)}
+                    clickSend={e => setIsChatBox(true)}
+                />)}
             </div>
 
             <div className='mt-5 text-right'>
