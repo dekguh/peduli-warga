67bfddcbbcc685da0c438fd7bdc9c9ae01f3218d
samyang lover <kadekteguhmahesa@gmail.com>
2021-09-18 14:53:02 +0800

    create team component

---
+++ src/components/organisms/Team/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import TitleSection from '../../atomics/text/TitleSection'
+
+const Team: React.FC = () => {
+    return (
+        <div className='container mx-auto'>
+            <TitleSection
+                title='Tim Kami'
+                subtitle='Kenali tim terbaik kami'
+                classes='mb-9'
+            />
+        </div>
+    )
+}
+
+export default Team
